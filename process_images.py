dc7954fa6dacba3155238ed1e8d41378b1891d12
ender <122208389+NotTrueFalse@users.noreply.github.com>
2024-01-28 17:19:30 +0100

    out -> temp

--- process_images.py
+++ process_images.py
@@ -1,14 +1,14 @@
 from pre_processing import preprocess_image
 import os
 i = 0
-l = len(os.listdir("out"))
-for img in os.listdir("out"):
+l = len(os.listdir("temp"))
+for img in os.listdir("temp"):
     i += 1
     if img.endswith(".png"):
         #reverse the color
-        result = preprocess_image('out/'+img)
+        result = preprocess_image('temp/'+img)
         result.save('captcha/'+img)
         try:
-            os.remove('out/'+img)
+            os.remove('temp/'+img)
         except:pass
         print(f"[*] {i} images processed {i}/{l}, {round(i/l*100, 2)}%"," "*20, end="\r")
